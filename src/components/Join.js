d1bd730dc44f0592d791c2a6e0eebff0f7c77e60
Connor Claxton <czclaxton@gmail.com>
2020-03-18 13:57:02 -0500

    update

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Join = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
-    <div>
-      <h1>Join</h1>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1>Join</h1>
+      </div>
     </div>
   );
 };
